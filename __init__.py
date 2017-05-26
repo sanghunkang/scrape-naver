4f4b8a15c1654fa318cbd30498ebf0bf0659c8b4
sanghunKang <sanghunkang.dev@gmail.com>
2017-05-27 01:32:39 +0900

    initial commit

---
+++ __init__.py
@@ -0,0 +1,159 @@
+import os, sys, time
+
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+import selenium
+
+# Define constants
+SYSTEM = sys.platform
+
+PATH_ARTICLES = '.\\results\\articles.txt'
+PATH_COMMENTS = '.\\results\\comments.txt'
+SPLITER = '||SPLITER||'
+
+def get_config(system):
+	ret = {}
+	if "win" in system:
+		path_config = "..\\.config\\Scrape_CafeNaver\\config"
+	else:
+		path_config = "../.config/Scrape_CafeNaver/config"
+
+	with open(path_config) as f:
+		ret["USER_ID"] = f.readline().strip()
+		ret["USER_PW"] = f.readline().strip()		
+	return ret
+
+# Import personal data to login
+USER_ID	= get_config(SYSTEM)["USER_ID"]
+USER_PW	= get_config(SYSTEM)["USER_PW"]
+
+# Define functions without return
+def initialize_file(path):
+	with open(path, 'w', encoding='utf-8') as fw:
+		pass
+
+def append_to_file(path, str_input):
+	with open(path, 'a', encoding='utf-8') as fw:
+		fw.write(str_input)
+
+def hard_input(id_elem, str_input):
+	elem = driver.find_element_by_id(id_elem)
+	actions = ActionChains(driver)
+	actions.move_to_element(elem)
+	actions.click()
+	actions.send_keys(str_input)  # Replace with whichever keys you want.
+	actions.perform()
+
+# def hard_trigger(elem):
+
+# def chain_find_element(driver_current, seq_locator):
+# 	locator = seq_locator.pop(0)
+
+# Define functions with return
+def wrap_entries(dict_entries, spliter):
+	ret = ''
+	for key in entries.keys():
+		ret = ret + entries[key] + spliter
+	ret = ret + '\n'
+	return ret
+
+# Run ########################################################################
+# Initialise output files
+initialize_file(PATH_ARTICLES)
+initialize_file(PATH_COMMENTS)
+
+# Launch the webdriver
+driver = webdriver.Chrome()
+
+# Attempt login
+driver.get('https://nid.naver.com/nidlogin.login')
+
+# Locate to account and password input boxes and enter account
+hard_input('id_area', USER_ID)
+hard_input('pw_area', USER_PW)
+
+# Trigger the submit button
+driver.find_element_by_class_name('btn_global').click()
+
+# Move to Eurang
+driver.get('http://cafe.naver.com/firenze')
+
+# Move to Reviewboard
+menulink = driver.find_element_by_id('menuLink59') #.click()
+driver.execute_script("arguments[0].click()", menulink)
+
+# Swith into inside iframe, where all relevant contents are
+main_area = driver.find_element_by_id('main-area')
+iframe = main_area.find_element_by_tag_name('iframe')
+driver.switch_to_frame(iframe)
+
+# Move to the first article from the list
+article_board = driver.find_elements_by_class_name('article-board')[1]
+article_board.find_element_by_class_name('aaa').click()
+
+while True:
+	cur = driver.find_element_by_class_name('list-btn-nor2')
+	cur = cur.find_element_by_class_name('fl')
+	cur = cur.find_elements_by_class_name('btn2')[-1]
+	cur.click()
+
+	try:
+		inbox = driver.find_element_by_class_name('inbox')
+
+		# ART_ID
+		cur = inbox.find_element_by_class_name('etc-box')
+		cur = cur.find_element_by_class_name('fr')
+		cur = cur.find_element_by_class_name('filter-50')
+		ART_ID = cur.text.split('/')[-1]
+
+		# ART_TITL
+		cur = inbox.find_element_by_class_name('tit-box')
+		cur = cur.find_element_by_class_name('fl')
+		cur = cur.find_element_by_class_name('m-tcol-c')
+		ART_TITL = cur.text
+		
+		# DATETIME
+		cur = inbox.find_element_by_class_name('tit-box')
+		cur = cur.find_element_by_class_name('fr')
+		DATETIME  = cur.find_element_by_class_name('m-tcol-c').text
+
+		# CONTENT
+		CONTENT = inbox.find_element_by_id('tbody').text
+		CONTENT = CONTENT.replace('\n', '&&\\n&&')
+
+		# Create an entry dictonary which will be later wrapped by wrap_entries()
+		entries = dict()
+		entries['ART_ID'] = ART_ID
+		entries['ART_TITL'] = ART_TITL
+		entries['DATETIME'] = DATETIME
+		entries['CONTENT'] = CONTENT
+
+		entries_wrapped = wrap_entries(entries, SPLITER)
+
+		# COMMENTS
+		COMMENTS = ''
+		seq_comment = inbox.find_elements_by_class_name('comm_body')
+		for comment in seq_comment:
+			comment_single = comment.text
+			comment_single = comment_single.replace('\n', '&&\\n&&')
+			print(comment.text)
+			COMMENTS = COMMENTS + ART_ID + SPLITER + comment_single + '\n'
+
+		print(COMMENTS)
+
+		append_to_file(PATH_ARTICLES, entries_wrapped)
+		append_to_file(PATH_COMMENTS, COMMENTS)
+
+	except selenium.common.exceptions.UnexpectedAlertPresentException:
+		print('Deleted or Non-existant page')
+		driver.switch_to_alert().accept()
+
+	driver.execute_script('window.localStorage.clear();')
+	# except selenium.common.exceptions.NoSuchElementException:
+	# 	print('Expected elements not Found: NoSuchElementException')
+
+# Write and save 
+
+driver.close()
+driver.quit()
+	
